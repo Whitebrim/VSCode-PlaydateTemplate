d9baf6acbca28509693ba9d77c2bfafab6266e3e
Денисов Ярослав <deni7off@yandex.ru>
2022-03-02 23:11:11 +0300

    Rewritten .cmd file that adds env variables

--- ADD_ENV_VARIABLE.cmd
+++ ADD_ENV_VARIABLE.cmd
@@ -1,3 +1,47 @@
-setx PLAYDATE_SDK_PATH "%userprofile%\Documents\PlaydateSDK\bin"
-RefreshEnv.cmd
+:: CHANGE PATH HERE
+:: %userprofile% is path to your user folder in C:/user/
+set SDKPATH="%userprofile%\Documents\PlaydateSDK"
+
+@echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+:: Add PLAYDATE_SDK_PATH env variable
+setx /M PLAYDATE_SDK_PATH "%SDKPATH%"
+:: Add pcd.exe to the PATH env variable
+set pathkey="HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment"
+for %%p in (pdc.exe) do (set pdcCheck=%%~$PATH:p)
+if not defined pdcCheck (
+   for /F "usebackq skip=2 tokens=2*" %%A IN (`reg query %pathkey% /v Path`) do (reg add %pathkey% /f /v Path /t REG_SZ /d "%%B;%SDKPATH%\bin")
+)
+:: Update register
+powershell -command "& {$md=\"[DllImport(`\"user32.dll\"\",SetLastError=true,CharSet=CharSet.Auto)]public static extern IntPtr SendMessageTimeout(IntPtr hWnd,uint Msg,UIntPtr wParam,string lParam,uint fuFlags,uint uTimeout,out UIntPtr lpdwResult);\"; $sm=Add-Type -MemberDefinition $md -Name NativeMethods -Namespace Win32 -PassThru;$result=[uintptr]::zero;$sm::SendMessageTimeout(0xffff,0x001A,[uintptr]::Zero,\"Environment\",2,5000,[ref]$result)}"
+echo SUCCESS!
 pause
+exit
--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 # Installation:
 0) **Unlock** `closeSim.ps1` file if it's locked: open properties and click unlock in the bottom of the window.
-1) If you've installed Playdate SDK to the default path (%userprofile%\Documents\PlaydateSDK\bin) then just **run** `ADD_ENV_VARIABLE.cmd` to add env variable to Playdate SDK. This should be done only once. If you've changed path - edit `ADD_ENV_VARIABLE.cmd`, change path to yours, then run it.
-2) Edit your `Code.exe` execatable to run with **admin rights** by default. You can find this file if you input this path to your explorer: `%appdata%\..\Local\Programs\Microsoft VS Code`
+1) If you've installed Playdate SDK to the default path (C:\user\%Username%\Documents\PlaydateSDK) then just **run** `ADD_ENV_VARIABLE.cmd` to add env variables:  
+* PLAYDATE_SDK_PATH to Playdate SDK  
+* Adds Playdate SDK's bin folder to PATH (if it is not already added) to create `pdc` shortcut  
+If you've changed default path - edit 3rd line in `ADD_ENV_VARIABLE.cmd`, then run it.  
+This should be done only once, you need to restart VSCode after this.
+2) Edit your `Code.exe` execatable (VSCode) to run with **admin rights** by default. You can find this file if you input this path to your explorer: `%appdata%\..\Local\Programs\Microsoft VS Code`
 3) Open template folder with VSCode, **install recomended extensions** (popup will show in the lower right corner): `Lua`, `Lua Plus`, `Playdate`. Then restart VSCode.
 4) If you want to change "build and run" key:
 * **Ctrl + K, Ctrl + S**
